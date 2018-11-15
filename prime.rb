96a13122cb9c0e9dfeeb55ae2434d80c23b676d7
Joseph Corona <josed.hc96@gmail.com>
2018-11-15 00:00:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  elsif int <= 3
+    return true
+  elsif int % 2 == 0 || int % 3 == 0
+    return false
+  end
+  i = 5
+  while i*i <= int do
+    if int % i == 0|| int % (i+2) == 0
+      return false
+    end
+    i += 6
+  end
+    true
+end
